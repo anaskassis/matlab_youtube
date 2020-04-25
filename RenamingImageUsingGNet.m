82875603f5e4e569cd98b5c9207e708c58c18f79
anaskassis <dranas2010@hotmail.com>
2020-04-24 17:33:46 -0700

    Update RenamingImageUsingGNet.m

--- RenamingImageUsingGNet.m
+++ RenamingImageUsingGNet.m
@@ -1,46 +1,20 @@
 net = googlenet;
 
 inputsize = net.Layers(1).InputSize;
-classNames = net.Layers(end).ClassNames;
 
-I= imread("sampleImages\agama90.jpg");
-figure, clf
-imshow(I)
-I = imresize(I,inputsize(1:2));
-figure,clf
-imshow(I)
+images = dir('images\*.jpg');
 
-[label,scores] = classify(net,I);
-
-[value,idx] = sort(scores,'descend');
-
-
-classNamesTop = classNames(idx(1:10));
-scoresTop = scores(idx(1:10));
-
-figure
-barh(scoresTop)
-xlim([0 1])
-title('Top 3 Predictions')
-xlabel('Probability')
-yticklabels(classNamesTop)
-
-
-
-files = dir('sampleImages\*.jpg');
-for i=1:length(files)
-    imageLocation  = [files(i).folder '\' files(i).name];
+for i=1:length(images)
+    imageLocation = [images(i).folder '\' images(i).name];
     I = imread(imageLocation);
     I = imresize(I,inputsize(1:2));
-    [label,scores]= classify(net,I);
-    %newName=label + i+ strrep(num2str(scores(classNames == label)),"0.","")+".jpg";
+    [label, score ] = classify(net,I);
     
-    newName=string(label) + i+ ".jpg";
-    newImageLocation=files(i).folder + "\" + newName;
+    newName = string(label) + i+".jpg";
+    newImagelocation = images(i).folder+"\"+newName;
+    movefile(imageLocation,newImagelocation)
     
-    movefile(imageLocation,newImageLocation)
     
 end
 
 
-net = alexnet;
